708def8e9d3b87d70ca839a510912e348b30817d
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-20 18:14:15 +0200

    remove data stubs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,55 +1,32 @@
 package controllers
 
-import play.api.Play.current
-import play.api._
-import play.api.mvc._
-import play.api.libs.iteratee.{ Iteratee, Enumerator, Concurrent }
-import libs.EventSource
 import concurrent.Future
+import play.api.libs.EventSource
+import play.api._
+import play.api.libs.iteratee.{ Iteratee, Enumerator, Concurrent }
+import play.api.mvc._
+import play.api.Play.current
 
 import akka.actor.Actor
 import akka.actor.Props
 import akka.event.Logging
 
-import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.concurrent.Akka
+import play.api.libs.concurrent.Execution.Implicits._
 
 import models.LichessStream
 import models.LichessStream._
-import chessmap.Lidata.{ Stub, StubActor, On, Off, Push }
 
 object Application extends Controller {
-
-  val (stubEnum, stubChannel) = Concurrent.broadcast[String]
-  val stubActor = Akka.system.actorOf(Props(new StubActor(stubChannel)))
 
   def index = Action {
     Ok(views.html.index())
   }
 
-  def infstub = Action {
-    stubActor ! Push
-    Ok.chunked(stubEnum)
-  }
-
-  def stub = Action {
-    Ok.chunked(Stub.enumerator)
-  }
-
-  def stubon = Action {
-    stubActor ! On
-    NoContent
-  }
-
-  def stuboff = Action {
-    stubActor ! Off
-    NoContent
-  }
-
   def stream = Action {
     val source = LichessStream.enumerator &> lineParser &>
-    toIpLocation &> toLocation &> withOpponentLocation &> 
-    asJson &> EventSource()
+      toIpLocation &> toLocation &> withOpponentLocation &>
+      asJson &> EventSource()
 
     Ok.chunked(source).as("text/event-stream")
   }
